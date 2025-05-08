3899cb9ef81d91ec6d0804a9d12d77537a82f114
Maxwell Cohen <to.email.max@gmail.com>
2025-05-08 09:51:18 -0500

    refactor(components): extract JSUsageSummary into a separate file and add @tanstack/react-table dependency
    Extract the JSUsageSummary component from CWVMetricsSummary.tsx into its own file to improve code modularity and maintainability. Additionally, add the @tanstack/react-table dependency to prepare for future table-related enhancements.

--- src/components/page-speed/CWVMetricsSummary.tsx
+++ src/components/page-speed/CWVMetricsSummary.tsx
@@ -3,8 +3,6 @@ import {
   AuditDetailTable,
   DebugData,
   NullablePageSpeedInsights,
-  TreeMapData,
-  TreeMapNode,
 } from '@/lib/schema';
 import {
   Table,
@@ -22,10 +20,11 @@ import {
   RenderBytesValue,
   RenderMSValue,
 } from './lh-categories/table/RenderTableValue';
-import { Fragment, JSX, useContext, useState } from 'react';
+import { Fragment, JSX, useContext } from 'react';
 import { groupBy } from '@/lib/utils';
 import { toTitleCase } from './toTitleCase';
 import { InsightsContext } from './PageSpeedContext';
+import { JSUsageSummary } from './JSUsageSummary';
 
 const CWV = [
   'firstContentfulPaint',
@@ -154,7 +153,7 @@ export function CWVMetricsSummary() {
           }}
         />
         <RenderNetworkRequestsSummary />
-        <RenderJSUsageSummary />
+        <JSUsageSummary />
         <RenderTable
           items={items}
           labels={labels}
@@ -263,125 +262,6 @@ function RenderNetworkRequestsSummary() {
   );
 }
 
-export function RenderJSUsageSummary() {
-  const items = useContext(InsightsContext);
-  const treeData = items
-    .map(({ item, label }) => ({
-      treeData: item.lighthouseResult?.audits?.['script-treemap-data']
-        ?.details as TreeMapData,
-      label,
-    }))
-    .filter(({ treeData }) => treeData?.type === 'treemap-data');
-  console.log('hi', treeData);
-  if (treeData.length === 0) return null;
-
-  return (
-    <>
-      {treeData.map(({ treeData, label }, idx) => {
-        return (
-          <Card className="sm:col-span-2 lg:col-span-full" key={`${idx}_label`}>
-            <CardHeader className="text-center text-2xl font-bold">
-              {label ? `JS Usage Summary for ${label}` : `JS Usage Summary`}
-            </CardHeader>
-            <Table className="border-2">
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="min-w-[18rem] max-w-[75vw] whitespace-nowrap">
-                    Name
-                  </TableHead>
-                  <TableHead> Resource Size</TableHead>
-                  <TableHead> Unused Bytes</TableHead>
-                  <TableHead> % of Unused Bytes</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {treeData.nodes.map((node, idx) => (
-                  <JSUsageTable
-                    key={`${idx}-${node.name}`}
-                    node={node}
-                    depth={0}
-                  />
-                ))}
-              </TableBody>
-            </Table>
-          </Card>
-        );
-      })}
-    </>
-  );
-}
-
-function JSUsageTable({
-  node,
-  depth = 0,
-}: {
-  node: TreeMapNode;
-  depth: number;
-}) {
-  const [open, setOpen] = useState(true);
-  const handleClick = () => {
-    setOpen(!open);
-  };
-  const unusedBytes = node.unusedBytes || 0;
-  const resourceBytes = node.resourceBytes || 0;
-  const percent = (unusedBytes / resourceBytes) * 100;
-  const isLarge = unusedBytes > 51200;
-  return (
-    <Fragment>
-      <TableRow onClick={handleClick} suppressHydrationWarning>
-        <TableCell
-          className={`min-w-[12rem] max-w-[75vw] overflow-scroll whitespace-nowrap ${depth ? 'border-b border-l' : ''}`}
-          style={{ width: `calc(75vw - ${0.5 * depth}rem) ` }}
-        >
-          <div className="flex flex-row align-middle">
-            {isLarge || percent > 50 ? <WarningSquare /> : null}
-            <div>
-              {`${node.name}${node.duplicatedNormalizedModuleName ? ` - ${node.duplicatedNormalizedModuleName}` : ''}`}
-            </div>
-          </div>
-        </TableCell>
-        <TableCell className="w-20 border">
-          {RenderBytesValue({
-            value: resourceBytes || 0,
-          })}
-        </TableCell>
-        <TableCell className="w-20 border">
-          {RenderBytesValue({
-            value: unusedBytes || 0,
-          })}
-        </TableCell>
-        <TableCell className={`w-20 ${depth ? 'border' : ''}`}>
-          {`${percent.toFixed(2)} %`}
-        </TableCell>
-      </TableRow>
-      {node.children && open ? (
-        <TableCell
-          className="border-x border-b-2 py-0 pr-0"
-          colSpan={4}
-          suppressHydrationWarning
-        >
-          <Table className="pr-0 pt-0" suppressHydrationWarning>
-            {node.children.map((child, idx) => (
-              <JSUsageTable
-                key={`${idx}-${child.name}`}
-                node={child}
-                depth={depth + 1}
-              />
-            ))}
-          </Table>
-        </TableCell>
-      ) : null}
-    </Fragment>
-  );
-}
-
-function WarningSquare() {
-  return (
-    <div className={'rounded-ful mr-2 h-3 w-3 self-center bg-yellow-500'}>
-      <span className="sr-only">warning this function could have extra JS</span>
-    </div>
-  );
-}
 
 function sumOn<T>(items: T[], key: string) {
   return items.reduce((acc, curr) => {
